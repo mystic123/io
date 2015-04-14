e49652f1e4eca9b63a894629004a7fd734c418c4
Piotr <akademia86@gmail.com>
2015-04-14 11:39:53 +0200

    test

--- server/src/dbcontroller.cpp
+++ server/src/dbcontroller.cpp
@@ -9,4 +9,3 @@ DBController::~DBController()
 {
 
 }
-
--- server/src/dbcontroller.h
+++ server/src/dbcontroller.h
@@ -1,12 +1,28 @@
 #ifndef DBCONTROLLER_H
 #define DBCONTROLLER_H
-
+//select for update pamietac!
+#include "event.h"
+#include "user.h"
 
 class DBController
 {
 public:
     DBController();
     ~DBController();
+
+    /* User controller */
+    int createUser(const User &u);
+    int updateUser(const User &u);
+    int removeUser(const User &u);
+    User getUserById(const uid_type);
+    User getUserByFbId(const uid_type);
+
+    /* Event controller */
+    int createEvent(const Event &e);
+    int updateEvent(const Event &e);
+    int removeEvent(const Event &e);
+    Event getEvent(const eid_type);
+
 };
 
 #endif // DBCONTROLLER_H
