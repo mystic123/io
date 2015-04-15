924d067c6b9ec11b27df2f8f295ee72c97a9c411
Pawel Kapica <pawel.kapica@gmail.com>
2015-04-15 22:06:03 +0200

    slightly modified event

--- shared/event.h
+++ shared/event.h
@@ -9,17 +9,18 @@ class Event
 public:
 	explicit Event(id_type);
 	Event();
-    explicit Event(const id_type, const id_type, const QString,const QList<id_type>&, const QList<id_type>&);
-//	explicit Event(QByteArray) throw(SerializationException&);
+	explicit Event(const id_type, const id_type, const QString,const QList<id_type>&, const QList<id_type>&);
+	//	explicit Event(QByteArray) throw(SerializationException&);
 	Event(id_type, QString, QDateTime, QList<id_type>);
-   virtual ~Event();
+	virtual ~Event();
 	id_type id() const { return _id; }
 	QString desc() const { return _desc; }
-    id_type creator() const { return _creator; }
+	QDateTime date() const { return _date; }
+	id_type creator() const { return _creator; }
 	QList<id_type> invited() const { return QList<id_type>(_invited); }
 	QList<id_type> attending() const { return QList<id_type>(_attending); }
 
-//   operator QByteArray const();
+	//   operator QByteArray const();
 
 	friend QDataStream& operator<<(QDataStream&, const Event&);
 	friend QDataStream& operator>>(QDataStream&, Event&);
@@ -27,11 +28,12 @@ public:
 private:
 	id_type _id;
 	id_type _creator;
-   QString _desc;
+	QString _desc;
+	QDateTime _date;
 	QList<id_type> _invited;
 	QList<id_type> _attending;
-   /* Location */
-   /* Comments */
+	/* Location */
+	/* Comments */
 
 	/* forbidden */
 	Event(const Event&);
