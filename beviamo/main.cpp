eb996557d96512aeb9786bb1f56ba0c8e86a43b4
js335769 <pjsieminski@gmail.com>
2015-05-05 11:36:40 +0200

    beviamo z zajec

--- beviamo/main.cpp
+++ beviamo/main.cpp
@@ -6,6 +6,25 @@
 #include "client.h"
 #include "event.h"
 
+QObject *dobra(QQmlApplicationEngine *engine)
+{
+    static Client *example = nullptr;
+    if (!example) {
+        example = new Client();
+        example->setEngine(engine);
+        example->login();
+    }
+    return example;
+}
+
+QObject *dobra2(QQmlEngine *engine, QJSEngine *scriptengine)
+{
+    Q_UNUSED(engine)
+    Q_UNUSED(scriptengine)
+
+    return dobra(nullptr);
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -13,11 +32,9 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/qrc/main.qml")));
 
-    Client c;
-    c.setEngine(&engine);
-    c.login();
-
-    //qmlRegisterType<Event>("Bev", 1, 0, "Event");
+    dobra(&engine);
+    qmlRegisterSingletonType<Client>("Bev", 1, 0, "Client", dobra2);
+    qmlRegisterType<Event>("Bev", 1, 0, "Event");
 
     return app.exec();
 }
--- beviamo/qrc/welcome.qml
+++ beviamo/qrc/welcome.qml
@@ -1,5 +1,6 @@
 import QtQuick 2.0
 
+
 Rectangle {
     id: rect
     color: "#000000"
